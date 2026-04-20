8155cfe4c646c3296d30b289688f06e59ae0fc41
gustavoLimaAraujo <gustavo.araujo@sptech.school>
2026-04-20 12:08:05 -0300

    adicionando novo componente reutilizável e melhorando a lógica, e adicionando nova tela

--- public/js/componentesReutilizaveis.js
+++ public/js/componentesReutilizaveis.js
@@ -1,34 +1,71 @@
 let header = document.querySelector('.header');
 let footer = document.querySelector('.footer');
+let headerHome = document.querySelector('.header_home');
 
-header.innerHTML = `
-    <header class="cor-primaria">
-        <a href="index.html">
-            <figure>
-                <img src="assets/imgs/solaire-icon.png" alt="Icon do Solaire">
-            </figure>
-        </a>
-        <nav>
-            <ul>
-                <li>
-                    <a href="historia.html">História do Jogo</a>
-                </li>
-                <li>
-                    <a href="sobre.html">Sobre o Jogo</a>
-                </li>
-                <li>
-                    <a href="cadastro.html">Cadastro</a>
-                </li>
-                <li>
-                    <a href="login.html">Login</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-`
+if(header != null){
+    header.innerHTML = `
+        <header class="cor-primaria">
+            <a href="index.html">
+                <figure>
+                    <img src="assets/imgs/solaire-icon.png" alt="Icon do Solaire">
+                </figure>
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="historia.html">História do Jogo</a>
+                    </li>
+                    <li>
+                        <a href="sobre.html">Sobre o Jogo</a>
+                    </li>
+                    <li>
+                        <a href="cadastro.html">Cadastro</a>
+                    </li>
+                    <li>
+                        <a href="login.html">Login</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    `
+}
 
-footer.innerHTML = `
-    <footer class="cor-primaria">
-        <h2>Todos os direitos reservados - © 2026 <a href="https://github.com/gustavoLimaAraujo">gustavoLimaAraujo</a></h2>
-    </footer>
-`
+if(headerHome != null){
+    headerHome.innerHTML = `
+        <header class="cor-primaria">
+            <a href="index.html">
+                <figure>
+                    <img src="assets/imgs/solaire-icon.png" alt="Icon do Solaire">
+                </figure>
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Desenvolvedor</a>
+                    </li>
+                    <li>
+                        <a href="#">Fórum</a>
+                    </li>
+                    <li>
+                        <a href="#">Dashboard</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <button onclick="window.location.href='index.html'">
+                <figure>
+                    <h2>Saída</h2>
+                    <img src="assets/imgs/icon-sair.png" alt="Icon de Sair">
+                </figure>
+            </button>
+        </header>
+    `
+}
+
+if(footer != null){
+    footer.innerHTML = `
+        <footer class="cor-primaria">
+            <h2>Todos os direitos reservados - © 2026 <a href="https://github.com/gustavoLimaAraujo">gustavoLimaAraujo</a></h2>
+        </footer>
+    `
+}
